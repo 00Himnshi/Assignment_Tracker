dd097d93d8d7d76e23d853253c82fa51828e2e37
Himnshi <143053261+00Himnshi@users.noreply.github.com>
2024-09-24 01:35:16 +0530

    Update send.py

--- send.py
+++ send.py
@@ -8,6 +8,6 @@ def send_message(update):
     message = client.messages.create(
     from_='whatsapp:+14155238886',
     body='Hii, A new update has been made on ERP portal{}'.format(update),
-    to='whatsapp:+917011678257'
+    to='whatsapp:+phone_number'
     )
 
